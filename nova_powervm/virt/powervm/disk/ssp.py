ea84362dba020a10952b01af246fa2c647f21066
Eric Fried <efried@us.ibm.com>
2015-03-06 16:51:07 -0600

    Fix oslo imports to new paths - ssp followon
    Followon to 162298 for new file ssp.py: update oslo.config import to
    oslo_config.
    
    Change-Id: Ibd63bd3a7cfef68a6b6337bfd4a203b91d092295

--- nova_powervm/virt/powervm/disk/ssp.py
+++ nova_powervm/virt/powervm/disk/ssp.py
@@ -14,7 +14,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from oslo.config import cfg
+from oslo_config import cfg
 import oslo_log.log as logging
 
 from nova import image
