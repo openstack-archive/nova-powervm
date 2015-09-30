a290c7acba7954e16623c8458d5fcd95bae5332e
Ravi Kota <ravkota3@in.ibm.com>
2015-09-30 10:08:21 -0500

    Support PowerVM attribute 'powervm:processor_compatibility'.
    Support PowerVM extra spec attribute 'powervm:processor_compatibility
    during the conversion of flavor from nova format to pypowervm format.
    
    Change-Id: If6f3bd7e9fa47d05dbe713e037ffb9e436e7e468

--- nova_powervm/tests/virt/powervm/test_vm.py
+++ nova_powervm/tests/virt/powervm/test_vm.py
@@ -100,6 +100,12 @@ class TestVMBuilder(test.TestCase):
         self.assertEqual(self.lpar_b._format_flavor(instance, flavor),
                          test_attrs)
 
+        # Test processor compatibility
+        flavor.extra_specs = {'powervm:processor_compatibility': 'POWER8'}
+        test_attrs = dict(lpar_attrs, **{'processor_compatibility': 'POWER8'})
+        self.assertEqual(self.lpar_b._format_flavor(instance, flavor),
+                         test_attrs)
+
         # Test min, max proc units
         flavor.extra_specs = {'powervm:min_proc_units': '0.5',
                               'powervm:max_proc_units': '2.0'}
