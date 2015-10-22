24dfec6c4be55a461e4e40c2869ffa75a445dbee
Adam Reznechek <adreznec@us.ibm.com>
2015-10-22 13:52:40 -0400

    Add base devstack plugins support
    Add support for installing ceilometer powervm through the devstack
    plugins infrastructure. This allows you to simply enable the plugin
    in a devstack localrc, provide any non-default config, and run
    stack.sh to configure/install it.
    - Added plugin files
    - Added README
    - Added bashate to tox test environments for scripts
    
    Change-Id: I4f95b710026a418bda07767f1e04ddb3fd855316

---
+++ devstack/README.rst
@@ -0,0 +1,25 @@
+========================
+Installing with Devstack
+========================
+
+1. Download DevStack::
+
+    $ git clone https://git.openstack.org/openstack-dev/devstack /opt/stack/devstack
+
+2. Modify DevStack's local.conf to pull in both Ceilometer this project by adding::
+
+    [[local|localrc]]
+    ...
+    enable_plugin ceilometer http://git.openstack.org/openstack/ceilometer
+    enable_plugin ceilometer-powervm http://git.openstack.org/openstack/ceilometer-powervm
+
+3. See ceilometer-powervm/doc/source/devref/usage.rst, then configure
+   the installation through options in local.conf as needed::
+
+    [[local|localrc]]
+    ...
+
+4. Run ``stack.sh`` from devstack::
+
+    $ cd /opt/stack/devstack
+    $ ./stack.sh
---
+++ devstack/plugin.sh
@@ -0,0 +1,124 @@
+#!/bin/bash
+#
+# plugin.sh - Devstack extras script to install and configure the ceilometer
+# hypervisor inspector for powervm
+
+# This driver is enabled in ceilometer.conf with:
+#   hypervisor_inspector=powervm
+
+# The following entry points are called in this order for ceilometer-powervm:
+#
+# - install_ceilometer_powervm
+# - configure_ceilometer_powervm
+# - start_ceilometer_powervm
+# - stop_ceilometer_powervm
+# - cleanup_ceilometer_powervm
+
+# Save trace setting
+MY_XTRACE=$(set +o | grep xtrace)
+set +o xtrace
+
+# Defaults
+# --------
+
+# Set up base directories
+CEILOMETER_CONF_DIR=${CEILOMETER_CONF_DIR:-/etc/ceilometer}
+CEILOMETER_CONF=${CEILOMETER_CONF:-CEILOMETER_CONF_DIR/ceilometer.conf}
+
+# ceilometer-powervm directories
+CEILOMETER_POWERVM_DIR=${CEILOMETER_POWERVM_DIR:-${DEST}/ceilometer-powervm}
+CEILOMETER_POWERVM_PLUGIN_DIR=$(readlink -f $(dirname ${BASH_SOURCE[0]}))
+
+# Source functions
+source $CEILOMETER_POWERVM_PLUGIN_DIR/powervm-functions.sh
+
+# Entry Points
+# ------------
+
+# configure_ceilometer_powervm() - Configure the system to use ceilometer_powervm
+function configure_ceilometer_powervm {
+    # Set the hypervisor inspector to be powervm
+    iniset $CEILOMETER_CONF DEFAULT hypervisor_inspector $HYPERVISOR_INSPECTOR
+}
+
+# install_ceilometer_powervm() - Install ceilometer_powervm and necessary dependencies
+function install_ceilometer_powervm {
+    install_pypowervm
+
+    # Install the ceilometer-powervm package
+    setup_develop $CEILOMETER_POWERVM_DIR
+}
+
+# start_ceilometer_powervm() - Start the ceilometer_powervm process
+function start_ceilometer_powervm {
+    # Check that NovaLink is installed and running
+    check_novalink_install
+
+    # Start the pvm ceilometer compute agent
+    run_process pvm-ceilometer-acompute "$CEILOMETER_BIN_DIR/ceilometer-polling --polling-namespaces compute --config-file $CEILOMETER_CONF"
+}
+
+# stop_ceilometer_powervm() - Stop the ceilometer_powervm process
+function stop_ceilometer_powervm {
+    # Stop the pvm ceilometer compute agent
+    stop_process pvm-ceilometer-acompute
+}
+
+# cleanup_ceilometer_powervm() - Cleanup the ceilometer_powervm process
+function cleanup_ceilometer_powervm {
+    # This function intentionally left blank
+    :
+}
+
+# Core Dispatch
+# -------------
+if is_service_enabled pvm-ceilometer-acompute; then
+    if [[ "$1" == "stack" && "$2" == "pre-install" ]]; then
+        # Install NovaLink if set
+        if [[ "$INSTALL_NOVALINK" == "True" ]]; then
+            echo_summary "Installing NovaLink"
+            install_novalink
+        fi
+
+        echo_summary "Configuring ceilometer services"
+        # Configure telemetry services
+        disable_service ceilometer-aipmi
+    fi
+
+    if [[ "$1" == "stack" && "$2" == "install" ]]; then
+        # Perform installation of ceilometer-powervm
+        echo_summary "Installing ceilometer-powervm"
+        install_ceilometer_powervm
+
+    elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
+        # Lay down configuration post install
+        echo_summary "Configuring ceilometer-powervm"
+        configure_ceilometer_powervm
+
+    elif [[ "$1" == "stack" && "$2" == "extra" ]]; then
+        # Initialize and start the PowerVM SEA agent
+        echo_summary "Starting ceilometer-powervm"
+        start_ceilometer_powervm
+    fi
+
+    if [[ "$1" == "unstack" ]]; then
+        # Shut down PowerVM SEA agent
+        echo_summary "Stopping ceilometer-powervm"
+        stop_ceilometer_powervm
+    fi
+
+    if [[ "$1" == "clean" ]]; then
+        # Remove any lingering configuration data
+        # clean.sh first calls unstack.sh
+        echo_summary "Cleaning up ceilometer-powervm and associated data"
+        cleanup_ceilometer_powervm
+        cleanup_pypowervm
+    fi
+fi
+
+# Restore xtrace
+$MY_XTRACE
+
+# Local variables:
+# mode: shell-script
+# End:
