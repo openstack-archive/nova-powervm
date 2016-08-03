fb05e31d9e397d1775a2802cc23c15e285d9ef07
Tyler Blakeslee <tblakes@us.ibm.com>
2016-08-03 14:52:56 -0500

    Added revert to delete LPAR from destination host on failed rebuild
    This change adds a revert method to the Create task. Originally
    there was no revert task for this method because LPAR cleanup
    was handled elsewhere on a failed build. On rebuilds however,
    this cleanup code was not being run because new builds and
    rebuilds follow different flows. This was causing future
    rebuilds to fail, because it appeared that the LPAR was already
    on the destination host. The added revert method checks to
    confirm that the failed build was a rebuild, and if it was,
    it removes the LPAR off the destination host.
    
    Change-Id: I56bae7e545f56d1fdf455309d11f24b61b0a9a1c
    Closes-Bug: #1596797

--- nova_powervm/virt/powervm/tasks/vm.py
+++ nova_powervm/virt/powervm/tasks/vm.py
@@ -25,6 +25,8 @@ from nova_powervm.virt.powervm.i18n import _LE
 from nova_powervm.virt.powervm.i18n import _LI
 from nova_powervm.virt.powervm import vm
 
+from nova.compute import task_states
+
 LOG = logging.getLogger(__name__)
 
 
@@ -60,10 +62,14 @@ class Create(task.Task):
                  nvram_mgr=None):
         """Creates the Task for creating a VM.
 
-        The revert method is not implemented because the compute manager
-        calls the driver destroy operation for spawn errors.  By not deleting
-        the lpar, it's a cleaner flow through the destroy operation and
-        accomplishes the same result.
+        The revert method only needs to do something for failed rebuilds.
+        Since the rebuild and build methods have different flows, it is
+        necessary to clean up the destination LPAR on fails during rebuild.
+
+        The revert method is not implemented for build because the compute
+        manager calls the driver destroy operation for spawn errors. By
+        not deleting the lpar, it's a cleaner flow through the destroy
+        operation and accomplishes the same result.
 
         Any stale storage associated with the new VM's (possibly recycled) ID
         will be cleaned up.  The cleanup work will be delegated to the FeedTask
@@ -102,6 +108,16 @@ class Create(task.Task):
         pvm_stg.add_lpar_storage_scrub_tasks([wrap.id], self.stg_ftsk,
                                              lpars_exist=True)
         return wrap
+
+    def revert(self, result, flow_failures, **kwargs):
+        # Only reverts failed rebuilds, because the revert
+        # for a failed build is handled in the manager.
+
+        if self.instance.task_state == task_states.REBUILD_SPAWNING:
+            LOG.info(_LI('Rebuild of instance %s failed. '
+                     'Deleting instance from destination.'),
+                     self.instance.name, instance=self.instance)
+            vm.dlt_lpar(self.adapter, vm.get_pvm_uuid(self.instance))
 
 
 class Resize(task.Task):
