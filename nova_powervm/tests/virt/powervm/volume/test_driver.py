cec1c996e67d90b75742d9e4b3f3ffc407e6831d
Drew Thorstensen <thorst@us.ibm.com>
2017-01-23 13:57:12 -0500

    Fix hostname & initiator for volume drivers
    This is a simple fix to make sure the iSCSI initiator is always returned
    and the host name is generally consistent between FC and iSCSI.
    
    Change-Id: I584b4ff4ea99b47a451e87c51c0c6ab4d0d28a0f
    Closes-Bug: 1658740

--- nova_powervm/tests/virt/powervm/volume/test_driver.py
+++ nova_powervm/tests/virt/powervm/volume/test_driver.py
@@ -15,8 +15,11 @@
 #    under the License.
 
 import mock
+from pypowervm.wrappers import virtual_io_server as pvm_vios
 
 from nova import test
+
+from nova_powervm.virt.powervm import volume
 
 
 class TestVolumeAdapter(test.TestCase):
@@ -32,3 +35,29 @@ class TestVolumeAdapter(test.TestCase):
         self.mock_inst_wrap = mock.MagicMock()
         self.mock_inst_wrap.can_modify_io.return_value = (True, None)
         self.mock_get_inst_wrap.return_value = self.mock_inst_wrap
+
+
+class TestInitMethods(test.TestCase):
+
+    @mock.patch('pypowervm.tasks.hdisk.discover_iscsi_initiator')
+    @mock.patch('pypowervm.tasks.partition.get_mgmt_partition')
+    def test_get_iscsi_initiator(self, mock_mgmt, mock_iscsi_init):
+        # Set up mocks and clear out data that may have been set by other
+        # tests
+        mock_adpt = mock.Mock()
+        mock_mgmt.return_value = mock.Mock(spec=pvm_vios.VIOS)
+        mock_iscsi_init.return_value = 'test_initiator'
+
+        self.assertEqual('test_initiator',
+                         volume.get_iscsi_initiator(mock_adpt))
+
+        # Make sure it gets set properly in the backend
+        self.assertEqual('test_initiator', volume._ISCSI_INITIATOR)
+        self.assertTrue(volume._ISCSI_LOOKUP_COMPLETE)
+        mock_mgmt.assert_called_once_with(mock_adpt)
+        self.assertEqual(1, mock_mgmt.call_count)
+
+        # Invoke again, make sure it doesn't call down to the mgmt part again
+        self.assertEqual('test_initiator',
+                         volume.get_iscsi_initiator(mock_adpt))
+        self.assertEqual(1, mock_mgmt.call_count)
