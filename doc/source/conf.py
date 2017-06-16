9420ea2c507a18a819edc885969265ea1e140ca1
Vu Cong Tuan <tuanvc@vn.fujitsu.com>
2017-06-16 20:50:07 +0700

    Fix html_last_updated_fmt for Python3
    html_last_updated_fmt option is interpreted as a
    byte string in python3, causing Sphinx build to break.
    This patch makes it utf-8 string.
    
    Changing Popen to .check_output because of 2 reasons:
    1. check_output() will raise CalledProcessError if
    the called process returns a non-zero return code.
    2. For consistency with keystone [1] and cinder [2]
    
    [1] https://review.openstack.org/#/c/457142
    [2] https://review.openstack.org/#/c/433081
    
    Change-Id: I810f841097c64c5e73db42ca393f6179182d9694

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -143,9 +143,8 @@ pygments_style = 'sphinx'
 # using the given strftime format.
 #html_last_updated_fmt = '%b %d, %Y'
 git_cmd = ["git", "log", "--pretty=format:'%ad, commit %h'", "--date=local",
-    "-n1"]
-html_last_updated_fmt = subprocess.Popen(
-    git_cmd, stdout=subprocess.PIPE).communicate()[0]
+           "-n1"]
+html_last_updated_fmt = subprocess.check_output(git_cmd).decode('utf-8')
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
